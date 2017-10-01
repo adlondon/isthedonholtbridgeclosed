2cdc917a4f24481d4b1e170b6c848354a73c3ecd
Andrew London <london.andrewd@gmail.com>
2017-10-01 19:44:01 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Router, Route, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = compose(applyMiddleware(reduxThunk)(createStore));
+const store = createStoreWithMiddleware(reducers);
+import './../sass/style.scss';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>
+  , document.querySelector('.page-container'));
