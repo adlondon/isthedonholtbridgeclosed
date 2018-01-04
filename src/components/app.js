d106a1aa6afb26acd05ea19e14dd1da6162def0d
Andrew London <london.andrewd@gmail.com>
2018-01-04 01:21:37 -0500

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -39,7 +39,7 @@ export default class App extends Component {
           {open
             ? <div className={`blue`}>OPEN<span className="subtext">(yes whackadoodles are still driving)</span></div>
             : <div className={`red`}>CLOSED</div>}</div>
-          <p>Idea from <a href="https://isthecooperriverbridgeclosed.com">isthecooperriverbridgeclosed.com</a> by Sean McCambridge. Photo by <a href="https://i.pinimg.com/originals/9b/5b/bd/9b5bbd3cf02efd9ff53cde48f0fcad63.jpg">Meredith Jones</a>. Website by <a href="mailto:london.andrewd@gmail.com">Andrew London</a>. <br /> Updated 1/3/2017 at 2:37 PM</p>
+          <p>Idea from <a href="https://isthecooperriverbridgeclosed.com">isthecooperriverbridgeclosed.com</a> by Sean McCambridge. Photo by <a href="https://i.pinimg.com/originals/9b/5b/bd/9b5bbd3cf02efd9ff53cde48f0fcad63.jpg">Meredith Jones</a>. Website by <a href="mailto:london.andrewd@gmail.com">Andrew London</a>. <br /> Updated 1/4/2017 at 1:21 AM</p>
       </div>
     );
   }
