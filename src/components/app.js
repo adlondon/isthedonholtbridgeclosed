abdbe2b335c44ebf7a9dd5d9ddb5496078052805
Andrew London <london.andrewd@gmail.com>
2019-03-18 11:22:19 -0400

    Update app.js

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      open: false
+      open: true
     };
   }
 
@@ -37,7 +37,7 @@ export default class App extends Component {
           {open
             ? <div className={`blue`}>OPEN</div>
             : <div className={`red`}>CLOSED<span className="subtext">(on the eastbound side. Don't even bother)</span></div>}</div>
-        <p>Stole the idea from <a href="https://isthecooperriverbridgeclosed.com">isthecooperriverbridgeclosed.com</a> by Sean McCambridge. Photo by <a href="https://i.pinimg.com/originals/9b/5b/bd/9b5bbd3cf02efd9ff53cde48f0fcad63.jpg">Meredith Jones</a>. Website by <a href="mailto:london.andrewd@gmail.com">Andrew London</a>. <br /> Updated 03/18/2019 at 9:29 AM.</p>
+        <p>Stole the idea from <a href="https://isthecooperriverbridgeclosed.com">isthecooperriverbridgeclosed.com</a> by Sean McCambridge. Photo by <a href="https://i.pinimg.com/originals/9b/5b/bd/9b5bbd3cf02efd9ff53cde48f0fcad63.jpg">Meredith Jones</a>. Website by <a href="mailto:london.andrewd@gmail.com">Andrew London</a>. <br /> Updated 03/18/2019 at 11:16 AM.</p>
       </div>
     );
   }
